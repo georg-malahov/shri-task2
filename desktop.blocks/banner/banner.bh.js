0df126ef8c25bcb47b2337cb1d78b4cc8cbac734
Юрий Малахов <yuri.malakhov87@gmail.com>
2015-08-16 21:52:02 +0300

    Add link to GitHub.

---
+++ desktop.blocks/banner/banner.bh.js
@@ -0,0 +1,13 @@
+module.exports = function (bh) {
+	bh.match('banner', function (ctx) {
+		ctx.content([
+			{
+				block : 'button',
+				mods : { theme : 'islands', size : 'm', type : 'link' },
+				attrs: { target: '_blank' },
+				url : 'https://github.com/yuriMalakhov/shri-task2',
+				text : 'Watch on GitHub'
+			}
+		], true);
+	});
+};
